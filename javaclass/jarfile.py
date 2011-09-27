ddab6dc901a882f8f02d2c45eaa0b21eb7766b96
David Drysdale <dmd@lurklurk.org>
2011-09-27 21:15:58 +0100

    Add code to load jarfile contents

---
+++ javaclass/jarfile.py
@@ -0,0 +1,27 @@
+import os
+import zipfile
+
+import classfile
+
+
+def jar_classes(filename):
+    """Return a list of the classes in a jar file.
+
+    Each entry is a 2-tuple of (filename, ClassFile)"""
+    zf = zipfile.ZipFile(filename, "r")
+    classes = []
+    for info in zf.infolist():
+        _, ext = os.path.splitext(info.filename)
+        if ext == ".class":
+            in_data = zf.open(info).read()
+            jc = classfile.ClassFile(in_data)
+            classes.append((info.filename, jc))
+    zf.close()
+    return classes
+
+if __name__ == "__main__":
+    import sys
+    for name in sys.argv[1:]:
+        jclasses = jar_classes(name)
+        for filename, jc in jclasses:
+            print filename, jc
--- makefile
+++ makefile
@@ -1,16 +1,25 @@
 TEST_JAVA_FILES = $(shell find tests -name \*.java)
 TEST_CLASS_FILES = $(TEST_JAVA_FILES:.java=.class)
 
-test: $(TEST_CLASS_FILES)
+test: testclasses testjar
+
+test.jar: $(TEST_CLASS_FILES)
+	jar -cf $@ $^
+
+testclasses:
 	@list='$(TEST_CLASS_FILES)'; for cfile in $$list; do \
 	  python javaclass/classfile.py $$cfile; \
 	done
+
+testjar: test.jar
+	python javaclass/jarfile.py $<
 
 %.class: %.java
 	javac $(TEST_JAVA_FILES)
 
 clean:
 	rm -rf build
+	rm -f test.jar
 	rm -f javaclass/*.pyc javaclass/*.py,cover
 	rm -f tests/*.class
 	rm -f tests/testpackage/*.class
